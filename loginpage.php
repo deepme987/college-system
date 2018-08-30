ef1a9631f603e630784cc7e599723e7ad646c436
deepme987 <deepme987@gmail.com>
2018-08-30 21:14:18 +0530

    Login Page Upload

---
+++ loginpage.php
@@ -0,0 +1,35 @@
+<html>
+<title>loginpage</title>
+<head>
+	<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+	<div class="block">
+		<div class="image">
+			<img src="images.jpg" alt="net change kar">
+		</div>
+		<div class="form">
+			<form action="" method="POST">
+				<div class="left">
+					<em> USER ID: </em>
+					<br><br>
+					<em> PASSWORD: </em>
+					<br><br>
+					<input type="submit" >
+				</div>
+				<div class="right">
+					<input type="text" id="id" placeholder="user_id">
+					<br><br>
+					<input type="password" id="pwd" placeholder="password">
+					<br><br>
+					<em> CANCEL </em>
+					<input type="button" id="cancel">
+				</div>
+			</form>
+		</div>
+	</div>
+
+</body>
+
+
+</html>
