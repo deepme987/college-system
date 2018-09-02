0672ea3d1e2417bd1410d153f69812dfe0244b78
mithil-jain <mithiljain8@gmail.com>
2018-09-02 21:07:43 +0530

    text

--- dashboard.php
+++ dashboard.php
@@ -21,13 +21,52 @@
 		$data = mysqli_query($conn, $get_all) or die("No records found.");
 
 		$row = mysqli_fetch_assoc($data);
-
+        $x=40;
+        $y=20;
 		echo'<div class="container">
-				<br><ul>
-				<li><span>'.$row["Fname"].' '.$row["Lname"].'</span><br></li>
-				<li>'.$row["UserId"].'</li>
-				<li><span>'.$row["Class"].' - '.$row["Division"].' - '.$row["RollNo"].'</span><br></li>
-		</div>';
+                <div class="details">
+                    <p>Details:<p>
+                    <hr>
+                    <ul class="left">
+                    <li>Name: </li>
+                    <li>smartcard no:</li>
+                    <li>class:</li>
+                    </ul>
+				    <ul class="right">
+				    <li>'.$row["Fname"].' '.$row["Lname"].'</li>
+				    <li>'.$row["UserId"].'</li>
+				    <li>'.$row["Class"].' - '.$row["Division"].' - '.$row["RollNo"].'</li>
+                    </ul>
+                </div>
+                <div class="attendance">
+                    <p>Attendance:</p>
+                    <hr>
+                    <table class="tattendance">
+                    <tr>
+                    <th>Total</th>
+                    <th>Total Attended</th>
+                    <th>Percentage</th>
+                    </tr>
+                    <tr>
+                    <td>'.$x.'</td>
+                    <td>'.$y.'</td>
+                    <td>'.($y/$x)*100 .'</td>
+                    </tr>
+                    </table>
+                </div>
+                <div class="notification">
+                    <p>Assignment Pending:</p>
+                    <hr>
+                    <p></p>
+                </div>
+                <div class="events">
+                    <p>Events:</p>
+                    <hr>
+                    <p></p>
+                </div>
+                    
+
+            </div>';
 
 		mysqli_close($conn);
 	?>
