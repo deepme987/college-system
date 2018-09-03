c6771459e3b2e0301faa327375d8e339fa4c95e4
deepme987 <deepme987@gmail.com>
2018-09-03 05:33:32 +0530

    Added bg

--- home.php
+++ home.php
@@ -49,7 +49,7 @@
                     <li><a class="btn <?php if($_GET["page"]=="assign.php"){echo 'active';}?>" href="#">Assignments</a></li>
                     <li><a class="btn <?php if($_GET["page"]=="events.php"){echo 'active';}?>" href="#">Events in college</a></li>
                     <li><a class="btn <?php if($_GET["page"]=="list.php"){echo 'active';}?>" href="home.php?page=list.php">Student List</a></li>
-                    <li><a class="btn <?php if($_GET["page"]=="tt.php"){echo 'active';}?>" href="#">Timetable</a></li>
+                    <li><a class="btn <?php if($_GET["page"]=="tt.php"){echo 'active';}?>" href="home.php?page=timetable.php">Timetable</a></li>
                     <li><a class="btn <?php if($_GET["page"]=="update.php"){echo 'active';}?>" href="home.php?page=update.php">My profile</a></li>
                 </ul>
             </div>
