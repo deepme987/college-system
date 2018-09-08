e5481aae126a1418d7bb58f72c553387a5b27cfd
deepme987 <deepme987@gmail.com>
2018-09-08 14:27:21 +0530

    events

---
+++ add_events.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Add Details</title>
+	<link rel="stylesheet" href="Addons/attendance.css">
+    
+</head>
+<body>		
+
+    <?php
+		if ($_SESSION['type']!="admin") {	
+			exit("Not enough privilege.");
+		}
+	?>
+	<p EventName="patten">Add Details</p>
+	<div EventName="form">
+        <div EventName="formsticky">
+		<form action="" method="POST">
+			<br>
+			<input type="number" name="EventId" placeholder="eventid">
+			<input type="date" name="date" placeholder="date">
+			<input type="time" name="time" placeholder="time">
+			<input type="text" name="venue" placeholder="venue">
+			<input type="text" name="EventName" placeholder="eventname">
+			<input type="submit" name="confirm" id="confirm" value="Add Values!">
+		</form> 
+        </div>
+
+		<?php
+
+			if (isset($_POST['confirm'])) {add();}
+			
+			function add() {
+
+				echo $_POST['time'];
+				$user = 'teacher';
+				$pass = 'root';
+				$db = 'students';
+
+				$conn = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect to server".$db);
+
+
+				$sql = "INSERT INTO `events`(`id`, `date`, `time`, `venue`, `event_name`) VALUES (".$_POST['EventId'].",".$_POST['date'].",".$_POST['time'].":00,\"".$_POST['venue']."\",\"".$_POST['EventName']."\")";
+
+				if (mysqli_query($conn, $sql)) {
+					echo "Added details, refreshing data...";
+				}
+				else {
+					echo "Error: ".mysqli_error($conn);
+				}
+
+				mysqli_close($conn);
+
+				header("refresh:3; url=http://localhost/Git/college-system/home.php?page=events.php");
+			}
+				
+		?>
+	</div>
--- list2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// include 'session.php';
-	if ($_SESSION['type']=="admin") {
-		include 'eventsadmin.php';
-	}
-	else {
-		include 'events.php';
-	}
-?>
