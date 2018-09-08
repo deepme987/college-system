c174fddf195809d018ebc8cd03db9895e4742317
mithil-jain <mithiljain8@gmail.com>
2018-09-08 14:59:10 +0530

    hello;

--- add_events.php
+++ add_events.php
@@ -13,9 +13,9 @@
 			exit("Not enough privilege.");
 		}
 	?>
-	<p EventName="patten">Add Details</p>
-	<div EventName="form">
-        <div EventName="formsticky">
+	<p class="patten">Add Details</p>
+	<div class="form">
+        <div class="formsticky">
 		<form action="" method="POST">
 			<br>
 			<input type="number" name="EventId" placeholder="eventid">
@@ -23,6 +23,7 @@
 			<input type="time" name="time" placeholder="time">
 			<input type="text" name="venue" placeholder="venue">
 			<input type="text" name="EventName" placeholder="eventname">
+            <input type="text" name="link" placeholder="link">
 			<input type="submit" name="confirm" id="confirm" value="Add Values!">
 		</form> 
         </div>
@@ -40,7 +41,7 @@
 				$conn = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect to server".$db);
 
 
-				$sql = "INSERT INTO `events`(`id`, `date`, `time`, `venue`, `event_name`) VALUES (".$_POST['EventId'].",".$_POST['date'].",\"".$_POST['time'].":00\",\"".$_POST['venue']."\",\"".$_POST['EventName']."\")";
+				$sql = "INSERT INTO `events`(`id`, `date`, `time`, `venue`, `event_name`) VALUES (".$_POST['EventId'].",".$_POST['date'].",\"".$_POST['time'].":00\",\"".$_POST['venue']."\",\"".$_POST['EventName']."\",\"".$_POST['link  ']."\")";
 
 				if (mysqli_query($conn, $sql)) {
 					echo "Added details, refreshing data...";
@@ -51,7 +52,7 @@
 
 				mysqli_close($conn);
 
-				header("refresh:3; url=http://localhost/Git/college-system/home.php?page=events.php");
+				header("refresh:3;url=http://localhost:8080/college-system/home.php?page=events.php");
 			}
 				
 		?>
