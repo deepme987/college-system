42ea5bf47c90188b9f43c95b5af02e43fc00cbfe
mithil-jain <mithiljain8@gmail.com>
2018-09-09 11:57:23 +0530

    hello

--- add_events.php
+++ add_events.php
@@ -22,18 +22,13 @@
 			<input type="date" name="date" placeholder="date">
 			<input type="time" name="time" placeholder="time">
 			<input type="text" name="venue" placeholder="venue">
-<<<<<<< HEAD
 			<input type="text" name="EventName" placeholder="eventname">
             <input type="text" name="link" placeholder="link">
-=======
-			<input type="text" name="EventName" placeholder="class">
->>>>>>> 4aef4a1a873a1037973f5120408bfb7cc498e178
-			<input type="submit" name="confirm" id="confirm" value="Add Values!">
+			<input type="submit" name="confirm" id="confirm" value="Add event!">
 		</form> 
         </div>
 
 		<?php
-
 			if (isset($_POST['confirm'])) {add();}
 			
 			function add() {
@@ -45,9 +40,10 @@
 				$conn = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect to server".$db);
 
 
-				$sql = "INSERT INTO `events`(`id`, `date`, `time`, `venue`, `event_name`) VALUES (".$_POST['EventId'].",".$_POST['date'].",\"".$_POST['time'].":00\",\"".$_POST['venue']."\",\"".$_POST['EventName']."\",\"".$_POST['link  ']."\")";
+				$sql = "INSERT INTO `events`(`id`, `date`, `time`, `venue`, `event_name`,`link`) VALUES (".$_POST['EventId'].",".$_POST['date'].",\"".$_POST['time'].":00\",\"".$_POST['venue']."\",\"".$_POST['EventName']."\",\"".$_POST['link  ']."\")";
 
 				if (mysqli_query($conn, $sql)) {
+                    $f=0;
 					echo "Added details, refreshing data...";
 				}
 				else {
@@ -55,15 +51,11 @@
 				}
 
 				mysqli_close($conn);
-
-<<<<<<< HEAD
-				header("refresh:3;url=http://localhost:8080/college-system/home.php?page=events.php");
-=======
-				header("refresh:3; url=http://localhost/Git/college-system/home.php?page=events.php");
-
-				// include 'check_events.php';
->>>>>>> 4aef4a1a873a1037973f5120408bfb7cc498e178
-			}
+                if($f==0)
+                {
+				header("refresh:10;url=http://localhost:8080/college-system/home.php?page=events.php");
+                }
+            }
 				
 		?>
 
@@ -82,7 +74,7 @@
 
 	        echo "<div class=\"table\"><table><tr><th>Event Id</th><th>Date</th><th>Time</th><th>venue</th><th>Event Name</th>";
 			while($row = mysqli_fetch_assoc($temp)) {
-				echo "<tr><td>".$row["id"]."</td><td>".$row["date"]."</td><td>".$row["time"]."</td><td>".$row["venue"]."</td><td><a style=\"text-decoration:none;\" href='#' target='_blank'>".$row["event_name"]."</td></tr>";
+				echo "<tr><td>".$row["id"]."</td><td>".$row["date"]."</td><td>".$row["time"]."</td><td>".$row["venue"]."</td><td><a style=\"text-decoration:none;\" href=\"".$row["link"]."\" target=\"_blank\">".$row["event_name"]."</td></tr>";
 			}
 	        echo "</table></div>"; 
 
